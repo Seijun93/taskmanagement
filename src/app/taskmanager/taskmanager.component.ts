54cc769d61512b315e8a388dde77de1ef1649b80
Daniel <daniel.doern@web.de>
2024-12-31 16:15:49 +0100

    PrimeNG19 installiert

--- src/app/taskmanager/taskmanager.component.ts
+++ src/app/taskmanager/taskmanager.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { NavigationComponent } from "./navigation/navigation.component";
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-taskmanager',
   imports: [
     NavigationComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ButtonModule,
   ],
   templateUrl: './taskmanager.component.html',
   styleUrl: './taskmanager.component.css'
